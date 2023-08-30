599af608e477943bd72a19e9a951a10fdbe389f9
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-30 18:15:06 +0200

    Check in postman. All correct

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     //
     public function index(){
         
-        $projects= Project::with('technologies', 'type')->paginate(20);
+        $projects= Project::with('technologies', 'type')->paginate(5);
 
         return response()->json($projects);
     }
